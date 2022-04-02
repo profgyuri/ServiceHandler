a6d9664943e4da943fc628a8b03c35505720da61
Turbék György <gyorgy.turbek@gmail.com>
2022-04-03 00:41:48 +0200

    Add project files.

---
+++ ServiceHandler.WPF/MainWindow.xaml.cs
@@ -0,0 +1,113 @@
+﻿using System.Linq;
+using System.ServiceProcess;
+using System.Windows;
+using Microsoft.Win32;
+
+namespace ServiceHandler.WPF;
+
+/// <summary>
+///     Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    private readonly ServiceController _serviceController;
+    private readonly ServiceManager _serviceManager;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        _serviceController = new ServiceController();
+        _serviceManager = new ServiceManager();
+    }
+
+    private void InstallButton_Click(object sender, RoutedEventArgs e)
+    {
+        _serviceManager.AddService(ServiceNameTextBox.Text, LocalPathTextBox.Text);
+
+        ServiceListView.Items.Add(ServiceNameTextBox.Text);
+    }
+
+    private void StartButton_Click(object sender, RoutedEventArgs e)
+    {
+        var name = ServiceListView.SelectedItem.ToString();
+
+        if (ServiceListView.Items.Count == 0 || string.IsNullOrWhiteSpace(name))
+        {
+            return;
+        }
+
+        _serviceController.ServiceName =
+            _serviceManager
+                .GetServices()
+                .FirstOrDefault(x => x.EndsWith(name))!;
+
+        if (_serviceController.Status == ServiceControllerStatus.Stopped)
+        {
+            _serviceController.Start();
+        }
+    }
+
+    private void StopButton_Click(object sender, RoutedEventArgs e)
+    {
+        var name = ServiceListView.SelectedItem.ToString();
+
+        if (ServiceListView.Items.Count == 0 || string.IsNullOrWhiteSpace(name))
+        {
+            return;
+        }
+
+        _serviceController.ServiceName =
+            _serviceManager
+                .GetServices()
+                .FirstOrDefault(x => x.EndsWith(name))!;
+
+        if (_serviceController.Status == ServiceControllerStatus.Running)
+        {
+            _serviceController.Stop();
+        }
+    }
+
+    private void DeleteButton_Click(object sender, RoutedEventArgs e)
+    {
+        var name = ServiceListView.SelectedItem.ToString();
+
+        if (ServiceListView.Items.Count == 0 || string.IsNullOrWhiteSpace(name))
+        {
+            return;
+        }
+
+        _serviceController.ServiceName =
+            _serviceManager
+                .GetServices()
+                .FirstOrDefault(x => x.EndsWith(name))!;
+
+        if (_serviceController.Status == ServiceControllerStatus.Running)
+        {
+            _serviceController.Stop();
+        }
+
+        ServiceListView.Items.Remove(name);
+
+        _serviceManager.RemoveService(name);
+    }
+
+    private void Window_Loaded(object sender, RoutedEventArgs e)
+    {
+        ServiceListView.Items.Clear();
+        ServiceListView.ItemsSource = _serviceManager.GetServices();
+    }
+
+    private void BrowseMenuItem_Click(object sender, RoutedEventArgs e)
+    {
+        var openFileDialog = new OpenFileDialog
+        {
+            Filter = "*.exe|*.dll",
+            InitialDirectory = "c:"
+        };
+
+        if (openFileDialog.ShowDialog() == true)
+        {
+            LocalPathTextBox.Text = openFileDialog.FileName;
+        }
+    }
+}
---
+++ ServiceHandler.WPF/ServiceManager.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+
+namespace ServiceHandler.WPF;
+
+internal class ServiceManager
+{
+    private const string ServicePrefix = "profgyuri.";
+    private const string Create = "create";
+    private const string Delete = "delete";
+
+    /// <summary>
+    ///     Tries to register a new windows service.
+    /// </summary>
+    /// <param name="serviceName">Name of the service to register.</param>
+    /// <param name="path">Local path of the .exe or .dll file to install the new service from.</param>
+    public void AddService(string serviceName, string path)
+    {
+        if (string.IsNullOrWhiteSpace(serviceName))
+        {
+            throw new ArgumentNullException(nameof(serviceName));
+        }
+
+        if (string.IsNullOrWhiteSpace(path) || !File.Exists(path))
+        {
+            throw new ArgumentNullException(nameof(path));
+        }
+
+        if (GetServices().Any(x => x == serviceName))
+        {
+            return;
+        }
+
+        var info = GetProcessInfo(Create, $"{ServicePrefix}{serviceName}");
+        info.Arguments += path;
+
+        var process = Process.Start(info);
+        process?.WaitForExit();
+    }
+
+    /// <summary>
+    ///     Stops a running windows service.
+    /// </summary>
+    /// <param name="serviceName">Name of the service to stop.</param>
+    /// <exception cref="ArgumentNullException">When <paramref name="serviceName" /> is not valid.</exception>
+    public void RemoveService(string serviceName)
+    {
+        if (string.IsNullOrWhiteSpace(serviceName))
+        {
+            throw new ArgumentNullException(nameof(serviceName));
+        }
+
+        if (GetServices().All(x => x != serviceName))
+        {
+            return;
+        }
+
+        var info = GetProcessInfo(Delete, $"{ServicePrefix}{serviceName}");
+
+        var process = Process.Start(info);
+        process?.WaitForExit();
+    }
+
+    public IEnumerable<string> GetServices()
+    {
+        var services =
+            ServiceController
+                .GetServices()
+                .Where(x => x.ServiceName.StartsWith(ServicePrefix))
+                .Select(x => x.ServiceName[(ServicePrefix.Length - 1)..]);
+
+        return services;
+    }
+
+    private ProcessStartInfo GetProcessInfo(string type, string serviceName)
+    {
+        return new ProcessStartInfo
+        {
+            UseShellExecute = true,
+            RedirectStandardOutput = false,
+            FileName = "powershell.exe",
+            Arguments = "sc.exe " + type + $" \"{serviceName}\"",
+            CreateNoWindow = true
+        };
+    }
+}
